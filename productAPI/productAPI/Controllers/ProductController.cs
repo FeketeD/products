c9590d9c4b1a06842676c6bccdb9f48402697ae3
Fekete Dániel <fdniel@mitiszkad.lan>
2023-09-14 09:09:49 +0200

    Updated version

--- productAPI/productAPI/Controllers/ProductController.cs
+++ productAPI/productAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static productAPI.DTO;
 
 namespace productAPI.Controllers
 {
@@ -7,5 +8,11 @@ namespace productAPI.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private static readonly List<productDTO2> products = new()
+        {
+            new productDTO2(Guid.NewGuid(), "termék1", 2500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new productDTO2(Guid.NewGuid(), "termék2", 3500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new productDTO2(Guid.NewGuid(), "termék3", 4500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow)
+        };
     }
 }
---
+++ productAPI/productAPI/DTO.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace productAPI
+{
+    public class DTO
+    {
+        public record productDTO2(Guid ID, string productNeve, int ProductAr, DateTimeOffset CreatedTime, DateTimeOffset ModifiedTime);
+    }
+}
